6826861c9f324829a5914f93757a3e893e8dda4f
Chidera Obinali <chideraobinali1@gmail.com>
2020-10-04 20:07:59 -0500

    finished exercise 1

--- chapter1_intro_setup/Exercise_1_Data_Types/Exercise_1_Data_Types/Program.cs
+++ chapter1_intro_setup/Exercise_1_Data_Types/Exercise_1_Data_Types/Program.cs
@@ -33,6 +33,29 @@ namespace Exercise_1_Data_Types
 
             Console.WriteLine("Awesome. Sounds like you've used " + mpg + "miles-per-gallon so far.");
 
+            string wonderland = "Alice was beginning to get" +
+                " very tired of sitting by her sister on the bank, " +
+                "and of having nothing to do: once or twice she had peeped " +
+                "into the book her sister was reading, but it had no pictures or" +
+                " conversations in it, 'and what is the use of a book,' thought Alice" +
+                " 'without pictures or conversation?'";
+
+            Console.WriteLine("Name a term found in the first sentence in 'Alice's Adventures in Wonderland'");
+
+            string term = Console.ReadLine().ToLower();
+
+            wonderland = wonderland.ToLower();
+
+            int search = wonderland.IndexOf(term);
+
+            Console.WriteLine(term + " was found at " + search);
+
+            int size = term.Length;
+
+            string NewWonderland = wonderland.Remove(search, size);
+
+            Console.WriteLine(NewWonderland);
+
         }
     }
 }
